c2e828c279c3f94b2768af0d88e93bda8dd65d8d
MatiasOrlando <matiassorlando@gmail.com>
2023-03-03 17:08:32 +0100

    update cors

--- server.js
+++ server.js
@@ -9,13 +9,7 @@ const cookies = require("cookie-parser");
 require("dotenv").config();
 
 app.use(cookies());
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+app.options("*", cors());
 
 app.use(
   cors({
