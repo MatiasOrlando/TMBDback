8ef04aadb0d9118cfd1d59f5a6171d180b311e1d
MatiasOrlando <matiassorlando@gmail.com>
2023-03-03 17:01:21 +0100

    server js updated

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(
   cors({
     // Si aún no tenes deployado tu front en origin va la url local.
     // Una vez que se deploye el front acá va esa url que te entrega.
-    origin: "https://tmdb-front-five.vercel.app/",
+    origin: "*",
     methods: ["GET", "POST", "DELETE", "OPTIONS"],
     credentials: true,
   })
