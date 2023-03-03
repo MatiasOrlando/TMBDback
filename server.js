16078bcb43626b7f3fc47058d1c2debcb07d5cab
MatiasOrlando <matiassorlando@gmail.com>
2023-03-03 16:51:32 +0100

    update server js

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ require("dotenv").config();
 
 app.use(cookies());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(
   cors({
     // Si aún no tenes deployado tu front en origin va la url local.
