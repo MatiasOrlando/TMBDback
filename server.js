ea9de5141f9ba3659065e6bd2cdb97faaa2e38a7
MatiasOrlando <matiassorlando@gmail.com>
2023-03-03 16:31:04 +0100

    restore server js

--- server.js
+++ server.js
@@ -10,21 +10,14 @@ require("dotenv").config();
 
 app.use(cookies());
 
-const corsOptions = {
-  origin: "*",
-  credentials: true,
-  methods: ["GET", "POST", "DELETE", "OPTIONS"],
-};
-
 app.use(
-  // cors({
-  //   // Si aún no tenes deployado tu front en origin va la url local.
-  //   // Una vez que se deploye el front acá va esa url que te entrega.
-  //   origin: "https://tmdb-front-five.vercel.app/",
-  //   methods: ["GET", "POST", "DELETE", "OPTIONS"],
-  //   credentials: true,
-  // })
-  cors(corsOptions)
+  cors({
+    // Si aún no tenes deployado tu front en origin va la url local.
+    // Una vez que se deploye el front acá va esa url que te entrega.
+    origin: "https://tmdb-front-five.vercel.app/",
+    methods: ["GET", "POST", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
 );
 app.use(express.json());
 app.use("/", router);
