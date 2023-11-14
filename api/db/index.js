0c808ff4574bc9023ffdc3cdec02cd613469fdeb
MatiasOrlando <matiassorlando@gmail.com>
2023-11-14 23:16:34 +0100

    test db connection

--- api/db/index.js
+++ api/db/index.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
-const db = new Sequelize(db_tmdb, process.env.USER_NAME, process.env.PASSWORD, {
-  host: process.env.HOST_NAME,
-  dialect: "postgres",
-  logging: false,
-});
+const db = new Sequelize(
+  "db_tmdb",
+  process.env.USER_NAME,
+  process.env.PASSWORD,
+  {
+    host: process.env.HOST_NAME,
+    dialect: "postgres",
+    logging: false,
+  }
+);
 
 module.exports = db;
